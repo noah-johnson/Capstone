5b9fc56367bbb3d34a96969ac3be8f8ed3bfcbfe
Noah Johnson <johnson.no@husky.neu.edu>
2018-10-03 17:03:30 -0400

    added sample spinner field

--- STB/NFC/app/src/main/java/com/slackpack/nfc/MainActivity.java
+++ STB/NFC/app/src/main/java/com/slackpack/nfc/MainActivity.java
@@ -14,9 +14,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements Listener, View.On
     public static final String TAG = MainActivity.class.getSimpleName();
 
     // Element objects
+    private Spinner mSpinnerTest;
     private EditText mEtResponderId;
     private EditText mEtFirstName;
     private EditText mEtLastName;
@@ -62,6 +65,15 @@ public class MainActivity extends AppCompatActivity implements Listener, View.On
     private void initViews() {
 
         // Initialize elements here using their id set in activity_main.xml
+        mSpinnerTest = (Spinner) findViewById(R.id.spinner_test);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.responder_level_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        mSpinnerTest.setAdapter(adapter);
+
         mEtResponderId = (EditText) findViewById(R.id.et_responder_id);
         mEtFirstName = (EditText) findViewById(R.id.et_patient_first_name);
         mEtLastName = (EditText) findViewById(R.id.et_patient_last_name);
